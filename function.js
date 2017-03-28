75b201cde09fb2472d44cc1d98d6de3340b13ae2
leeya018 <leeyahav018@gmail.com>
2017-03-28 14:25:22 +0300

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,55 @@
 
-function increaseSizeByOne(){}
+function moveRightInterval(){
+	snake1.direction =3;
+	snake1.moveRight();
+}
+
+
+function moveUpInterval(){
+	snake1.direction =2;
+	snake1.moveUp();
+}
+
+
+function moveLeftInterval(){
+	snake1.direction =1;
+	snake1.moveLeft();
+}
+
+
+function moveDownInterval(){
+	snake1.direction =4;
+	snake1.moveDown();
+}
+
+function canSnakeEatLeft(snakeHead,food){
+
+	return snakeHead.x -20 == food.x && snakeHead.y==food.y;
+}
+
+function canSnakeEatUp(snakeHead,food){
+	 
+	 return snakeHead.x == food.x && snakeHead.y -20 == food.y;
+	
+}
+
+function canSnakeEatRight(snakeHead,food){
+
+	 return snakeHead.x +20 == food.x && snakeHead.y==food.y;
+}
+
+function canSnakeEatDown(snakeHead,food){
+
+	 return snakeHead.x == food.x && snakeHead.y +20 == food.y;
+	
+}
+
+
+
+
+
+
+
 
 function CheckIfcrushTheWall(){}
 
