9a640498d6e4489994dbb17db7103d9937d8c2be
leeya018 <leeyahav018@gmail.com>
2017-03-28 14:24:01 +0300

    Update Food.js

--- Food.js
+++ Food.js
@@ -29,16 +29,32 @@ var types = {"blue":i,"red":d,"black":i,"darkviolet":d,"violet":i};
 	 return key;
 
 }
+
+function crashLimits(snakeBody,foodPosition){
+	for (var i = 0; i < snakeBody.length; i++) {
+		if(snakeBody[0].equals(foodPosition)){
+			return true;
+		}
+	}
+	return false;
+
+}
 //================================================================FOOD=====================
-var food1 = new food();
+
 
 function food(){
 
-this.draw = function(){
+this.draw = function(snakeBody){
+
 	this.position = randomPosition();
+	while(crashLimits(snakeBody,this.position)){
+		this.position = randomPosition();
+	}
+
 	this.color = randomcolor();
 	this.power = randomPower(this.color);
 	this.position.drawFood(this.color);
+	return this.position;
 }
 
 
@@ -50,15 +66,17 @@ this.draw = function(){
 
 
 
+
+
  function randomPosition(){
 
 	var random = Math.floor(Math.random()*870); 
-	var modulu = random%10;
+	var modulu = random%20;
 	var randomX = random - modulu;
 
 
 	random = Math.floor(Math.random()*380); 
-	modulu = random%10;
+	modulu = random%20;
 	var randomY = random - modulu;
 
 	return new position(randomX,randomY); 
