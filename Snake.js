3fd0ebe068e3e5a73f26cb7e05df37b7857c8910
leeya018 <leeyahav018@gmail.com>
2017-03-29 03:04:06 +0300

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -4,7 +4,9 @@ var snake1 = new snake("green");
 
 function snake(color){
 	this.color = color;
+	this.speed = 100;
 	this.direction=0; //1-left  2-up 3-right 4-down
+	this.barrier=[];
 	this.food;
 
 	this.arraySnake = [];
@@ -15,21 +17,21 @@ function snake(color){
 
 		switch(this.direction){
 			case 1://left
-			return canSnakeEatLeft(this.arraySnake[0],this.food);
+			return canSnakeEatLeft(this.arraySnake[0],this.food.position);
 			break;
 
 			case 2://up
-			return canSnakeEatUp(this.arraySnake[0],this.food);
+			return canSnakeEatUp(this.arraySnake[0],this.food.position);
 
 			break;
 
 			case 3://right
-			return canSnakeEatRight(this.arraySnake[0],this.food);
+			return canSnakeEatRight(this.arraySnake[0],this.food.position);
 
 			break;
 
 			case 4://down
-			return canSnakeEatDown(this.arraySnake[0],this.food);
+			return canSnakeEatDown(this.arraySnake[0],this.food.position);
 
 			break;
 
@@ -38,8 +40,14 @@ function snake(color){
 	}
 
 	this.createFood = function(){
-		var foodTemp = new food();
-		this.food =  foodTemp.draw(this.arraySnake);
+
+
+		this.food = new food();
+		while(this.food.crashLimits())
+		{
+			this.food = new food();
+		}
+		this.food.draw();
 
 	}
 
@@ -50,7 +58,7 @@ function snake(color){
 
 		switch(this.direction){
 			case 1://left
-			x = head.x +step;
+			x = head.x -step;
 			y= head.y;
 			break;
 
@@ -60,7 +68,7 @@ function snake(color){
 			break;
 
 			case 3://right
-			x = head.x -step;
+			x = head.x +step;
 			y= head.y;
 			break;
 
@@ -106,7 +114,8 @@ function snake(color){
 
 		if(this.canSnakeEat()){
 			console.log("I can eat"+ this.direction);
-			this.increaseSizeByOne();
+			// this.increaseSizeByOne();
+			this.food.randomPower();
 			this.createFood();
 		}
 	}
@@ -121,37 +130,73 @@ this.replacePosition = function(x,y){
 
 	}
 
+
+	this.checkCrushInTail = function(){
+		for (var i = 1; i < this.arraySnake.length; i++) {
+			if(this.arraySnake[0].equals(this.arraySnake[i])){
+				alert("game over");
+				return true;
+			}
+		}
+		return false;
+	}
+
+	this.checkCrushInBarrier = function(){
+		for (var i = 1; i < this.barrier.length; i++) {
+			if(this.arraySnake[0].equals(this.barrier[i])){
+				alert("stuck in Barrier");
+				return true;
+			}
+		}
+		return false;
+
+	}
+
 	this.moveRight = function(){
+		var ans= this.checkCrushInTail();
+		this.checkCrushInBarrier();
 		this.eatingCheck();
 		var tempPos = this.arraySnake[0];
-		var x = tempPos.x+step;
+		var x = (tempPos.x+step)%widthScreen;
 		var y = tempPos.y;
 		this.replacePosition(x,y);
 
 	}
 
 	this.moveLeft = function(){
+		this.checkCrushInTail();
+		this.checkCrushInBarrier();
 		this.eatingCheck();
 		var tempPos = this.arraySnake[0];
-		var x = tempPos.x-step;
+		var x = (tempPos.x-step)%widthScreen;
+		if(x<0){
+			x+=widthScreen;
+		}
 		var y = tempPos.y;
 		this.replacePosition(x,y);
 	}
 
 
 	this.moveUp = function(){
+		this.checkCrushInTail();
+		this.checkCrushInBarrier();
 		this.eatingCheck();
 		var tempPos = this.arraySnake[0];
 		var x = tempPos.x;
-		var y = tempPos.y-step;
+		var y = (tempPos.y-step)%heightScreen;
+		if(y<0){
+			y+=heightScreen;
+		}
 		this.replacePosition(x,y);
 	}
 
 	this.moveDown = function(){
+		this.checkCrushInTail();
+		this.checkCrushInBarrier();
 		this.eatingCheck();
 		var tempPos = this.arraySnake[0];
 		var x = tempPos.x;
-		var y = tempPos.y+step;
+		var y = (tempPos.y+step)%heightScreen;
 		this.replacePosition(x,y);
 	}
 
@@ -167,6 +212,40 @@ this.replacePosition = function(x,y){
 
 	
 }
+
+//=============================================================
+
+var intervalId;
+		var intervalFood = setInterval(snake1.createFood,2000);
+		$(document).keydown(function(e) {
+	 // alert((e.keyCode));
+	 $("#instructionRel").css("visibility","hidden");
+	 switch(e.which) {
+
+        case 37: // left
+        clearInterval(intervalId);
+        intervalId= setInterval(moveLeftInterval,snake1.speed);
+        break;
+
+        case 38: // up
+        clearInterval(intervalId);
+        intervalId = setInterval(moveUpInterval,snake1.speed);
+        break;
+
+        case 39: // right
+        clearInterval(intervalId);
+        intervalId = setInterval(moveRightInterval,snake1.speed);
+        break;
+
+        case 40: // down
+        clearInterval(intervalId);
+        intervalId = setInterval(moveDownInterval,snake1.speed);
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
 
 
 
